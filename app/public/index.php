d4a8cea2317fb653cce51d71a544d7c4731ba399
Yanko Ivanov <y.ivanov@codecoda.com>
2019-12-16 17:19:05 +0200

    - fixed a typo in the host env variable

--- app/public/index.php
+++ app/public/index.php
@@ -27,7 +27,7 @@ $loader
 $di->set('db', function () {
     return new MysqlAdapter(
         [
-            'host' => getenv('MSQL_HOST'),
+            'host' => getenv('MYSQL_HOST'),
             'username' => getenv('MYSQL_USER'),
             'password' => getenv('MYSQL_PASSWORD'),
             'dbname' => getenv('MYSQL_DATABASE'),
